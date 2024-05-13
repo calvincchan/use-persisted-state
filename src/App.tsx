8ca18763a2c5fea8e03e0390fffea37646ad5b17
Calvin C Chan <hello@calvin-c.com>
2024-05-13 16:26:16 -0700

    chore: clean page for example code

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text1, setText1] = useState("");
+  const [text2, setText2] = useState("");
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h3>Use Persisted State</h3>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <label>Text1:</label>
+        <input
+          type="text"
+          value={text1}
+          onChange={(e) => setText1(e.target.value)}
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="card">
+        <label>text2:</label>
+        <textarea
+          value={text2}
+          onChange={(e) => setText2(e.target.value)}
+        ></textarea>
+      </div>
+      <div className="card">
+        <button>Clear</button>
+        <button>Save</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
